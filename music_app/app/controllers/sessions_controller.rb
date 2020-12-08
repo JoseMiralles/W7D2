e23baaf8278cb7319364583427d108f4692a4bdc
Jose Miralles <josemiralles1991@gmail.com>
2020-12-08 16:19:47 -0500

    Fixed session create redirect

--- music_app/app/controllers/sessions_controller.rb
+++ music_app/app/controllers/sessions_controller.rb
@@ -13,7 +13,7 @@ class SessionsController < ApplicationController
         if @user
             @user.reset_session_token!
             session[:session_token] = @user.session_token
-            redirect_to users_url
+            redirect_to user_url(@user.id)
         else
             render :new
         end
